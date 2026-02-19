0dca3d1441ef6915b3946b0e258a02e1113f382f
SebastianAG7 <edwin.sp723@gmail.com>
2026-02-19 14:13:44 -0500

    Update cache version from v1 to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mandamientos-v1";
+const CACHE = "mandamientos-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
